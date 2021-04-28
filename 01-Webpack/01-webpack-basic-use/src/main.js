c0dc847a5765afa862565c89885e2aa84851605f
Zhao YongChi <lwyeah@qq.com>
2021-04-28 20:30:57 +0800

    VueProduction initialize

---
+++ 01-Webpack/01-webpack-basic-use/src/main.js
@@ -0,0 +1,10 @@
+const {add, mul} = require('./mathUtils')
+
+console.log(add(10, 20))
+console.log(mul(10, 20))
+
+import {name, age, height} from "./info.js";
+
+console.log(name);
+console.log(age);
+console.log(height);
